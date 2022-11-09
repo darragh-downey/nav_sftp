f56e78300737cff6b03b028e0da13d8eac9084d9
Darragh Downey <darragh.downey90@gmail.com>
2022-11-09 14:51:21 +1100

    to merge

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use nav_sftp::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err|{
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem while parsing command line arguments: {err}");
         process::exit(1);
     });
